f7fb24d7742fa2dc1492422cc657692f5d9d4d00
chuckborris89 <boris.fuchs89@gmail.com>
2018-11-28 16:53:25 +0100

    update timeofflight + upload ultrasonic sensor

--- exercises/proj02/code/TimeOfFlight/TimeOfFlight.ino
+++ exercises/proj02/code/TimeOfFlight/TimeOfFlight.ino
@@ -26,11 +26,11 @@ void loop() {
   Serial.print("Reading a measurement... ");
   lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
-  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+ // if (measure.RangeStatus != 4) {  // phase failures have incorrect data
     Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
-  } else {
-    Serial.println(" out of range ");
-  }
+ // } else {
+ //   Serial.println(" out of range ");
+ // }
     
   delay(100);
 }
